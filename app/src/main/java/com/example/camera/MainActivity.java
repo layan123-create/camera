8d0ae1394d80ffac9af9d8e12b07099a96801605
layan123-create <layanawad123123@gmail.com>
2025-01-29 00:12:26 +0200

    first commit

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.camera;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +16,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private EditText nameIn;
+    private EditText surnameIn;
+    private String camIn;
+    private EditText addressIn;
+    private Button confirmButton,changePictureButton;
+    private String name,surname,cam,address;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        nameIn = findViewById(R.id.editName);
+        surnameIn = findViewById(R.id.editSurName);
+        camIn = String.valueOf(findViewById(R.id.editNum));
+        addressIn = findViewById(R.id.editAdd);
+        confirmButton = findViewById(R.id.buttonConfirm);
+
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = nameIn.getText().toString();
+                surname = surnameIn.getText().toString();
+                camIn=camIn.getText().toString();
+                address = addressIn.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, secondActivity.class);
+                intent.putExtra("name", name);
+                intent.putExtra("surname", surname);
+                intent.putExtra("phone", cam);
+                intent.putExtra("address", address);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/camera/secondActivity.java
@@ -0,0 +1,76 @@
+package com.example.camera;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+public class secondActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Button BackButton;
+    private String details;
+    private ImageView imageView;
+    private Bitmap imageBitmap;
+    private String name,surname,cam,address,de;
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+        Button changePictureButton = findViewById(R.id.buttonChange);
+        changePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+            }
+        });
+        details = findViewById(R.id.textViewD);
+        BackButton = findViewById(R.id.buttonBack);
+        imageView = findViewById(R.id.imageView);
+        
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+        surname = intent.getStringExtra("surname");
+        cam = intent.getStringExtra("phone");
+        address = intent.getStringExtra("address");
+
+        details = "Name: " + name + "\nSurname: " + surname + "\nCam: " + cam + "\nAddress: " + address;
+        details.setText(de);
+        imageBitmap = (Bitmap) intent.getParcelableExtra("image");
+        if (imageBitmap != null) {
+            imageView.setImageBitmap(imageBitmap);
+
+            BackButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras=data.getExtras();
+            imageBitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
